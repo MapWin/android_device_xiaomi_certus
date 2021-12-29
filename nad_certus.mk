cd3be277c5196dcd85e7d7d1aef9b04587460a65
MapWin_ <68777300+MapWin@users.noreply.github.com>
2021-12-29 14:29:56 +0300

    aosp_

--- nad_certus.mk
+++ nad_certus.mk
@@ -18,17 +18,16 @@
 $(call inherit-product, device/xiaomi/certus/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common.mk)
 
 # Official tag
-NAD_BUILD_TYPE := OFFICIAL
 
 # Boot animaton
 TARGET_BOOT_ANIMATION_RES := 720
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := certus
-PRODUCT_NAME := nad_certus
+PRODUCT_NAME := aosp_certus
 PRODUCT_BRAND := xiaomi
 PRODUCT_MODEL := Redmi 6 / Redmi 6A
 PRODUCT_MANUFACTURER := Xiaomi
